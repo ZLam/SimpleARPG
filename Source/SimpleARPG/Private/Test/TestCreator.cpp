bd6eb313e22f51214d59debf09704edde167e803
ZLam <578058407@qq.com>
2020-05-29 15:17:32 +0800

    update~~~

--- Source/SimpleARPG/Private/Test/TestCreator.cpp
+++ Source/SimpleARPG/Private/Test/TestCreator.cpp
@@ -6,7 +6,6 @@
 #include "WVModule/Public/Logger/WVLog.h"
 #include "WVModule/Public/EventSys/WVEventDispatcher.h"
 #include "WVModule/Public/View/WVViewMgr.h"
-#include "Engine/Engine.h"
 
 void UTestCreator::OnEnter()
 {
@@ -15,26 +14,17 @@ void UTestCreator::OnEnter()
 
 void UTestCreator::Rec_CreateTestView()
 {
-	WVLogI(TEXT("111"))
 	auto cls = LoadClass<UTestUserWidget>(nullptr, TEXT("WidgetBlueprint'/Game/Test/BP_TestMainView.BP_TestMainView_C'"));
 	if (cls)
 	{
-		WVLogI(TEXT("222"))
-
-
-
 		auto w =	_gameIns->GetWorld();
 		if (w)
 		{
-			WVLogI(TEXT("333"))
-
 			auto view = CreateWidget<UTestUserWidget>(w, cls);
 			if (view)
 			{
-				WVLogI(TEXT("444"))
-					UWVViewMgr::GetInstance()->SwitchMainView(view);
+				UWVViewMgr::GetInstance()->SwitchMainView(view);
 			}
 		}
-
 	}
 }
